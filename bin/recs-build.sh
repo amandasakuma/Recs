0fff8c97ca3b2836222b77159a456b7a7019cfe6
Amanda Sakuma <amandasakuma@gmail.com>
2022-10-26 11:23:47 -0400

    redeploying frontend

--- bin/recs-build.sh
+++ bin/recs-build.sh
@@ -5,5 +5,9 @@ set -o errexit
 bundle install
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+bundle install
 bundle exec rake db:migrate 
-bundle exec rake db:seed
